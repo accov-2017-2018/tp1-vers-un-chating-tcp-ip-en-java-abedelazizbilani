b655de811bc414c99e7d1b749f9606b8ba72da61
abed <abed.bilani@gmail.com>
2018-03-18 19:17:32 +0200

    server client chat multi thread first push

--- codeInitialpropose/ClientTCP/src/Clienttcp/ClientTCP.java
+++ codeInitialpropose/ClientTCP/src/Clienttcp/ClientTCP.java
@@ -12,47 +12,60 @@ import java.net.Socket;
  * @author Pascal Fares
  */
 public class ClientTCP {
+    
+    public static final int port = 4444;
 
     /**
-     * Récupère le flux d'entrée d'une Socket et l'encapsule dans un BufferedReader 
-     * Un BufferedReader permet de Lire le texte à partir d'un flux d'entrée de caractères, en mettant
-     * en mémoire tampon les caractères afin de permettre une lecture efficace 
-     * des caractères, des tableaux et des lignes.
+     * Récupère le flux d'entrée d'une Socket et l'encapsule dans un
+     * BufferedReader Un BufferedReader permet de Lire le texte à partir d'un
+     * flux d'entrée de caractères, en mettant en mémoire tampon les caractères
+     * afin de permettre une lecture efficace des caractères, des tableaux et
+     * des lignes.
+     *
      * @param p la SOcket
      * @return le BufferedReader crée
-     * @throws IOException 
+     * @throws IOException
      */
-    private static BufferedReader getInput(Socket p) throws IOException {
-        return new BufferedReader(new InputStreamReader(p.getInputStream()));
+    private static BufferedReader getInput() throws IOException {
+        return new BufferedReader(new InputStreamReader(System.in));
     }
-  
+    
+    private static BufferedReader getServerMessage(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(
+                socket.getInputStream()));
+    }
+
     /**
-     * Récupère le flus de sortie de la socket et l'encapsule dans un PrintWriter
-     * Imprime des représentations formatées d'objets dans un 
-     * flux de sortie de texte. Cette classe implémente toutes les méthodes 
+     * Récupère le flus de sortie de la socket et l'encapsule dans un
+     * PrintWriter Imprime des représentations formatées d'objets dans un flux
+     * de sortie de texte. Cette classe implémente toutes les méthodes
      * d'impression trouvées dans PrintStream.
+     *
      * @param p la Socket
      * @return le PrintWriter crée
-     * @throws IOException 
+     * @throws IOException
      */
-    private static PrintWriter getoutput(Socket p) throws IOException{
-        return new PrintWriter (new OutputStreamWriter(p.getOutputStream()));
+    private static PrintWriter getOutput(Socket socket) throws IOException {
+        return new PrintWriter(socket.getOutputStream(), true);
     }
+
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         //Se connecté au port 2000
-        Socket l = new Socket("localhost",2000);
-        System.out.println(l.getLocalSocketAddress());
-        //
-        BufferedReader entreeSock = getInput(l);
-        PrintWriter sortieSock = getoutput(l);
-        //Envoyer une chaine
-        sortieSock.printf("Bonjour\n");sortieSock.flush();
-        //Récupérer la réponse puis l'afficher
-        System.out.println(entreeSock.readLine());
+//        String name = args[0];
+        Socket socket = new Socket("localhost", port);
+        while (true) {
+            String reader = getInput().readLine();
+            getOutput(socket).println(reader);
+            String messageFromServer = getServerMessage(socket).readLine();
+            whileChatting(messageFromServer);
+        }
     }
     
+    private static void whileChatting(String messageFromServer) {
+        System.out.println("Message from server to client " + messageFromServer);
+    }
 }
--- codeInitialpropose/ServerTCP/src/servertcp/ServerTCP.java
+++ codeInitialpropose/ServerTCP/src/servertcp/ServerTCP.java
@@ -7,60 +7,30 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Pascal Fares
  */
 public class ServerTCP {
-/**
-     * Récupère le flux d'entrée d'une Socket et l'encapsule dans un BufferedReader 
-     * Un BufferedReader permet de Lire le texte à partir d'un flux d'entrée de caractères, en mettant
-     * en mémoire tampon les caractères afin de permettre une lecture efficace 
-     * des caractères, des tableaux et des lignes.
-     * @param p la SOcket
-     * @return le BufferedReader crée
-     * @throws IOException 
-     */
-    private static BufferedReader getInput(Socket p) throws IOException {
-        return new BufferedReader(new InputStreamReader(p.getInputStream()));
-    }
-/**
-     * Récupère le flus de sortie de la socket et l'encapsule dans un PrintWriter
-     * Imprime des représentations formatées d'objets dans un 
-     * flux de sortie de texte. Cette classe implémente toutes les méthodes 
-     * d'impression trouvées dans PrintStream.
-     * @param p la Socket
-     * @return le PrintWriter crée
-     * @throws IOException 
-     */
-    private static PrintWriter getoutput(Socket p) throws IOException {
-        return new PrintWriter(new OutputStreamWriter(p.getOutputStream()));
-    }
+
+    public static final int port = 4444;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        //Créer une écoute sur le port 2000
-        ServerSocket l = new ServerSocket(2000);
-        System.out.println(l.getLocalSocketAddress());
-        while (true) {
-            //try-with-resource
-            try (Socket serviceSocket = l.accept()) {
-                //Acceper un client l.accept()
-                System.out.println(serviceSocket.getRemoteSocketAddress());
-                BufferedReader ir = getInput(serviceSocket);
-                PrintWriter reply = getoutput(serviceSocket);
-                String line;                
-                while ((line = ir.readLine()) != null) {                  
-                    System.out.printf("je répond ping %s\n", line);
-                    reply.printf("je répond ping %s\n", line);
-                    reply.flush();
-                }
-                //clore la socket (réalisé par try-with resource
-            }
-        }
+        new ServerTCP().runServer();
     }
 
+    public void runServer() throws IOException {
+        ServerSocket ss = new ServerSocket(port);
+        System.out.println("Server ready");
+        while (true) {
+            Socket socket = ss.accept();
+            new ServerThread(socket).start();
+        }
+    }
 }
